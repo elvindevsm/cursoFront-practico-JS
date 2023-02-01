0b962bb53f4b5054612c4c8dd99d12f4b529c357
Elvin <elvindeveloperSm@gmail.com>
2023-02-01 16:01:16 -0500

    Añadi menus,  funcionalidad para despliegue de menu, y media Querys para Responsive Desing

--- fuxxion.js
+++ fuxxion.js
@@ -1,6 +1,13 @@
 const activador = document.querySelector('.navbar-email');
 const Menucito =  document.querySelector('.desktop-menu ');
+const menuBurguer =  document.querySelector('.menu');
+const mobileMenu =  document.querySelector('.mobile-menu');
 
 activador.addEventListener('click', ()=>{
     Menucito.classList.toggle('inactive');
 });
+
+menuBurguer.addEventListener('click', ()=>{
+    mobileMenu.classList.toggle('inactive');
+});
+
